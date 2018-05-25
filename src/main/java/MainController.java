46610d1af033ad76486803fe45139e39ce718163
Philippe Li <philo1196@gmail.com>
2018-05-24 23:48:36 -0400

    use @RestController annotation

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,15 +1,13 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class MainController {
 
     @RequestMapping
-    @ResponseBody
     String home() {
         return "Hello world!";
     }
