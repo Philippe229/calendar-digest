a9c1665d916baec1c7ba43cae3047d9a99d2b696
Philippe Li <philo1196@gmail.com>
2018-05-27 21:57:55 -0400

    added index page

--- src/main/java/amazonsales/MainController.java
+++ src/main/java/amazonsales/MainController.java
@@ -1,14 +1,8 @@
 package amazonsales;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-
-    @RequestMapping("/")
-    String index() {
-        return "Hello world!";
-    }
 
 }
