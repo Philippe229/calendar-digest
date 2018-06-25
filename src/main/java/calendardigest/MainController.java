330f44c49a5372c30486763a2fa60da10396bcc6
Philippe Li <philo1196@gmail.com>
2018-06-25 19:14:21 -0400

    added authorization request for Gmail API

--- src/main/java/calendardigest/MainController.java
+++ src/main/java/calendardigest/MainController.java
@@ -1,8 +1,5 @@
 package calendardigest;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
@@ -15,9 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
@@ -26,31 +20,16 @@ import static org.quartz.TriggerBuilder.newTrigger;
 public final class MainController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
-    private final OAuthManager oAuthManager;
     private final Scheduler scheduler;
 
     @Autowired
-    public MainController(final OAuthManager oAuthManager,
-                          final Scheduler scheduler) {
-        this.oAuthManager = oAuthManager;
+    public MainController(final Scheduler scheduler) {
         this.scheduler = scheduler;
     }
 
     @RequestMapping(value = "/submit", method = RequestMethod.POST)
     public String submit(@RequestBody String body) {
         System.out.println(body);
-
-        // Build a new authorized API client service.
-        try {
-            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            oAuthManager.requestAuthorization(HTTP_TRANSPORT);
-        } catch (GeneralSecurityException | IOException e) {
-            LOGGER.error("Failed to create HTTP transport.", e);
-            System.exit(-1);
-        }
-
-        final Credential credential = oAuthManager.getCredential();
-        System.out.println(credential.getExpiresInSeconds());
 
         // Test task scheduler
         try {
--- src/main/java/calendardigest/OAuthManager.java
+++ src/main/java/calendardigest/OAuthManager.java
@@ -5,54 +5,83 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.gmail.GmailScopes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
 public final class OAuthManager {
 
     /**
-     * Directory to store user credentials
+     * Directory to store Gmail user credentials
      */
-    private static final String CREDENTIALS_FOLDER = "credentials";
+    private static final String GMAIL_CREDENTIALS_FOLDER = "credentials/gmail";
 
     /**
-     * File where client ID and client secret are stored
+     * Directory to store calendar user credentials
      */
-    private static final String CLIENT_SECRET_DIR = "client_secret.json";
+    private static final String CALENDAR_CREDENTIALS_FOLDER = "credentials/calendar";
+
+    /**
+     * File where client ID and client secret for Gmail API are stored
+     */
+    private static final String GMAIL_CLIENT_SECRET = "gmail_client_secret.json";
+
+    /**
+     * File where client ID and client secret for calendar API are stored
+     */
+    private static final String CALENDAR_CLIENT_SECRET = "calendar_client_secret.json";
 
     /**
      * Global instance of the scopes required by this app.
      * If modifying these scopes, delete previously saved credentials/ folder.
      */
-    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final List<String> GMAIL_SCOPES = Collections.singletonList(GmailScopes.GMAIL_COMPOSE);
+    private static final List<String> CALENDAR_SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OAuthManager.class);
     private final JsonFactory jsonFactory;
-    private GoogleClientSecrets clientSecrets;
-    private Credential credential;
+
+    private GoogleClientSecrets gmailClientSecrets;
+    private GoogleClientSecrets calendarClientSecrets;
+    private Credential gmailCredential;
+    private Credential calendarCredential;
 
     public OAuthManager() {
         jsonFactory = JacksonFactory.getDefaultInstance();
+    }
+
+    @PostConstruct
+    public void init() {
         loadClientSecrets();
+        requestAuthorization();
     }
 
     private void loadClientSecrets() {
-        final InputStream inputStream = Application.class
+        final InputStream calendarInputStream = getClass()
                 .getClassLoader()
-                .getResourceAsStream(CLIENT_SECRET_DIR);
+                .getResourceAsStream(CALENDAR_CLIENT_SECRET);
+
+        final InputStream gmailInputStream = getClass()
+                .getClassLoader()
+                .getResourceAsStream(GMAIL_CLIENT_SECRET);
         try {
-            clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(inputStream));
+            calendarClientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(calendarInputStream));
+            gmailClientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(gmailInputStream));
             LOGGER.info("Client secrets loaded.");
         } catch (IOException e) {
             LOGGER.error("Failed to load client secrets.", e);
@@ -60,26 +89,50 @@ public final class OAuthManager {
         }
     }
 
-    public void requestAuthorization(final NetHttpTransport httpTransport) {
+    private void requestAuthorization() {
+        final String userId = "testUser";
+        NetHttpTransport httpTransport = null;
+        try {
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        } catch (GeneralSecurityException | IOException e) {
+            LOGGER.error("Failed to create HTTP transport.", e);
+            System.exit(-1);
+        }
+
         try {
             // Build flow and trigger user authorization request.
-            final GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
-                    .Builder(httpTransport, jsonFactory, clientSecrets, SCOPES)
-                    .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(CREDENTIALS_FOLDER)))
+            LOGGER.info("Requesting authorization for calendar.");
+            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
+                    .Builder(httpTransport, jsonFactory, calendarClientSecrets, CALENDAR_SCOPES)
+                    .setDataStoreFactory(new FileDataStoreFactory(new File(CALENDAR_CREDENTIALS_FOLDER)))
                     .setAccessType("offline")
                     .build();
 
-            final LocalServerReceiver localServerReceiver = new LocalServerReceiver();
-            final AuthorizationCodeInstalledApp authorizationCode = new AuthorizationCodeInstalledApp(flow, localServerReceiver);
-            credential = authorizationCode.authorize("testUser");
-            LOGGER.info("testUser" + " authorized.");
+            AuthorizationCodeInstalledApp authorizationCode = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver());
+            calendarCredential = authorizationCode.authorize(userId);
+
+            LOGGER.info("Requesting authorization for Gmail.");
+            flow = new GoogleAuthorizationCodeFlow
+                    .Builder(httpTransport, jsonFactory, gmailClientSecrets, GMAIL_SCOPES)
+                    .setDataStoreFactory(new FileDataStoreFactory(new File(GMAIL_CREDENTIALS_FOLDER)))
+                    .setAccessType("offline")
+                    .build();
+
+            authorizationCode = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver());
+            gmailCredential = authorizationCode.authorize(userId);
+
+            LOGGER.info(userId + " authorized.");
         } catch (IOException e) {
             LOGGER.error("Failed to request user authorization.", e);
         }
     }
 
-    public Credential getCredential() {
-        return credential;
+    Credential getCalendarCredential() {
+        return calendarCredential;
+    }
+
+    Credential getGmailCredential() {
+        return gmailCredential;
     }
 
 }
