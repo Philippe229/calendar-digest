41f3fe2a0815e7357d28540bdf210c937fdbe4e8
Philippe Li <philo1196@gmail.com>
2018-07-07 00:06:11 -0400

    revert accidental changes

--- src/main/java/calendardigest/Application.java
+++ src/main/java/calendardigest/Application.java
@@ -24,32 +24,32 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-//        ApplicationContext context = AppContextProvider.getApplicationContext();
-//
-//        Scheduler scheduler = context.getBean(Scheduler.class);
-//        try {
-//            scheduler.start();
-//
-//            JobDetail job = newJob(SendEmailJob.class)
-//                    .withIdentity("job1", "group1")
-//                    .build();
-//
-//            final LocalDate localDate = LocalDate.now();
-//            final LocalDate sunday = localDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
-//            final Date date = Date.from(sunday.atStartOfDay(ZoneId.systemDefault()).toInstant());
-//            final long oneWeekInHours = TimeUnit.DAYS.toHours(7);
-//
-//            Trigger trigger = newTrigger()
-//                    .withIdentity("trigger1", "group1")
-//                    .startAt(new Date(date.getTime() + TimeUnit.MINUTES.toMillis(15)))
-//                    .withSchedule(SimpleScheduleBuilder
-//                            .repeatHourlyForever((int) oneWeekInHours))
-//                    .build();
-//
-//            scheduler.scheduleJob(job, trigger);
-//        } catch (SchedulerException e) {
-//            e.printStackTrace();
-//        }
+        ApplicationContext context = AppContextProvider.getApplicationContext();
+
+        Scheduler scheduler = context.getBean(Scheduler.class);
+        try {
+            scheduler.start();
+
+            JobDetail job = newJob(SendEmailJob.class)
+                    .withIdentity("job1", "group1")
+                    .build();
+
+            final LocalDate localDate = LocalDate.now();
+            final LocalDate sunday = localDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+            final Date date = Date.from(sunday.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            final long oneWeekInHours = TimeUnit.DAYS.toHours(7);
+
+            Trigger trigger = newTrigger()
+                    .withIdentity("trigger1", "group1")
+                    .startAt(new Date(date.getTime() + TimeUnit.MINUTES.toMillis(15)))
+                    .withSchedule(SimpleScheduleBuilder
+                            .repeatHourlyForever((int) oneWeekInHours))
+                    .build();
+
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
     }
 
 }
