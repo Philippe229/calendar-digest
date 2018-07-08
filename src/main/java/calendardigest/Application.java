a14a5ea3cac259ca9d415bacf0d6017d1449fd82
Philippe Li <philo1196@gmail.com>
2018-07-08 02:11:28 -0400

    changed time zone for job trigger

--- src/main/java/calendardigest/Application.java
+++ src/main/java/calendardigest/Application.java
@@ -5,6 +5,8 @@ import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SimpleScheduleBuilder;
 import org.quartz.Trigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -22,6 +24,8 @@ import static org.quartz.TriggerBuilder.newTrigger;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         ApplicationContext context = AppContextProvider.getApplicationContext();
@@ -34,14 +38,23 @@ public class Application {
                     .withIdentity("job1", "group1")
                     .build();
 
+            final ZoneId zoneId = ZoneId.of("America/Montreal");
             final LocalDate localDate = LocalDate.now();
             final LocalDate sunday = localDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
-            final Date date = Date.from(sunday.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            final Date dateMontreal = Date.from(sunday.atStartOfDay(zoneId).toInstant());
+            final Date dateUTC = null;
+            final Date date = new Date(dateMontreal.getTime() + TimeUnit.MINUTES.toMillis(15));
+
             final long oneWeekInHours = TimeUnit.DAYS.toHours(7);
+
+            LOGGER.info("ZoneId: " + zoneId.getId());
+            LOGGER.info("Digest will first be sent on " + sunday.toString());
+            LOGGER.info("Montreal time: " + dateMontreal.toString());
+            LOGGER.info("Digest will be sent at time: " + date.toString());
 
             Trigger trigger = newTrigger()
                     .withIdentity("trigger1", "group1")
-                    .startAt(new Date(date.getTime() + TimeUnit.MINUTES.toMillis(15)))
+                    .startAt(dateMontreal)
                     .withSchedule(SimpleScheduleBuilder
                             .repeatHourlyForever((int) oneWeekInHours))
                     .build();
