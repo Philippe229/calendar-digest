ef008f232d38ab4d273d0037911671d77599760a
Philippe Li <philo1196@gmail.com>
2018-07-23 20:47:16 -0400

    added settings page

--- src/main/java/calendardigest/MainController.java
+++ src/main/java/calendardigest/MainController.java
@@ -4,12 +4,12 @@ import org.quartz.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public final class MainController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
@@ -18,6 +18,14 @@ public final class MainController {
     @Autowired
     public MainController(final Scheduler scheduler) {
         this.scheduler = scheduler;
+    }
+
+    @RequestMapping({
+            "/",
+            "/settings"
+    })
+    public String redirectOnReload() {
+        return "forward:/index.html";
     }
 
     @RequestMapping(value = "/submit", method = RequestMethod.POST)
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,7 +2,7 @@
 var app = angular.module('app', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
-    $routeProvider.when('/', {
-        templateUrl: 'pages/login.html'
-    })
+    $routeProvider
+        .when('/', {templateUrl: 'pages/login.html'})
+        .when('/settings', {templateUrl: 'pages/settings.html'})
 });
